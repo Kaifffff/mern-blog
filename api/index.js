1e611bdd941ea457d21821f7d52b30dcfe8bdbb2
kaif <shaikhkaif3258@gmail.com>
2024-12-29 22:00:51 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from 'express';
+import { connect } from 'http2';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+main()
+.then(()=>{
+    console.log("connected successfuly");
+})
+.catch((err) =>{
+     console.log(err)
+    }
+);
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/mern-blog');
+}
 
 app.listen(3000 , ()=>{
     console.log('server is running on port 3000');
